fea9f3e8093f03bf1683784ef7ef027a1586d044
Ben Thomas <ben.thomas1984@gmail.com>
2018-02-03 12:15:26 +0000

    Basic application skeleton

---
+++ CMake/resolute-deps.cmake
@@ -0,0 +1,21 @@
+find_package(Boost 1.54.0 MODULE
+  COMPONENTS
+    chrono
+    date_time
+    filesystem
+    program_options
+    regex
+    system
+    thread
+  REQUIRED
+)
+
+include_directories(${Boost_INCLUDE_DIRS})
+link_directories(${Boost_LIBRARY_DIRS})
+
+find_package(ITK REQUIRED)
+include(${ITK_USE_FILE})
+
+find_package(glog REQUIRED)
+
+find_package(nlohmann_json)
