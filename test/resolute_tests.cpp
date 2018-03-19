3416ff388af10e8af79f48c36aed1dbd4e264841
Ben Thomas <ben.thomas1984@gmail.com>
2018-03-19 18:58:52 +0000

    Tests for R2* to HU/LAC conversion

--- test/resolute_tests.cpp
+++ test/resolute_tests.cpp
@@ -23,8 +23,30 @@
 
 namespace {
 
+TEST(Resolute, R2StoHU){
+   EXPECT_NEAR(330, ns::GetHUfromR2s(100.0), 10);
+   EXPECT_NEAR(615, ns::GetHUfromR2s(200.0), 10);
+   EXPECT_NEAR(844, ns::GetHUfromR2s(300.0), 10);
+   EXPECT_NEAR(1025, ns::GetHUfromR2s(400.0), 10);
+   EXPECT_NEAR(1166, ns::GetHUfromR2s(500.0), 10);
+   EXPECT_NEAR(1275, ns::GetHUfromR2s(600.0), 10);
+   EXPECT_NEAR(1360, ns::GetHUfromR2s(700.0), 10);
+   EXPECT_NEAR(1430, ns::GetHUfromR2s(800.0), 10);
+   EXPECT_NEAR(1492, ns::GetHUfromR2s(900.0), 10);
+   EXPECT_NEAR(1556, ns::GetHUfromR2s(1000.0), 10);
+}
+
 TEST(Resolute, R2StoLAC){
-   EXPECT_NEAR(0.115, ns::GetMU(100.0), 0.002);
+   EXPECT_NEAR(0.115, ns::GetMU(100.0), 0.001);
+   EXPECT_NEAR(0.129, ns::GetMU(200.0), 0.001);
+   EXPECT_NEAR(0.141, ns::GetMU(300.0), 0.001);
+   EXPECT_NEAR(0.150, ns::GetMU(400.0), 0.001);
+   EXPECT_NEAR(0.158, ns::GetMU(500.0), 0.001);
+   EXPECT_NEAR(0.163, ns::GetMU(600.0), 0.001);
+   EXPECT_NEAR(0.167, ns::GetMU(700.0), 0.001);
+   EXPECT_NEAR(0.171, ns::GetMU(800.0), 0.001);
+   EXPECT_NEAR(0.174, ns::GetMU(900.0), 0.001);
+   EXPECT_NEAR(0.177, ns::GetMU(1000.0), 0.001);
 }
 
 }
